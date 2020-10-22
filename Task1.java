7fcf27aa38abe4c4d42f446a79c43c65c21b8a63
Azamat-Musin <54332698+Azamat-Musin@users.noreply.github.com>
2020-10-22 18:08:11 +0300

    delete all useless objects in Task1.java

--- Task1.java
+++ Task1.java
@@ -4,15 +4,6 @@ class Task1{
 		if(args.length>0){
 			int t = Integer.valueOf(args[0]);
 			double g = 9.806;
-			String s = "Strange happens something here";
-			int aaa = 1000;
-			int bbbb = 100004;
-			int cccc = aaa + bbbb;
-			int arr[] = new int[10];
-			for (int i = 0; i < 10; i++) {
-				arr[i] = (int)(Math.random()*1000);
-			}
-			System.out.println(Arrays.toString(arr));
 			double answer = g*t*t/2;
 			System.out.println("The deep is " + answer + " meters");
 		}
